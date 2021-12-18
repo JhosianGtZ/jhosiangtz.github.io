23f620fe533f71b7574f0751371f73c4c4afe1d7
Jhosian <jhosianlpz@gmail.com>
2021-12-17 21:44:28 -0600

    Typing bug solved

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 let button = document.getElementById("icon");
 let header_links = document.getElementById("nav-links");
 let menu = document.getElementById("menu");
@@ -8,37 +6,37 @@ let counter = 0;
 let cerrado = 0;
 
 button.addEventListener("click", function () {
-    if (counter == 0) {
-        menu.classList.remove('bx-menu-alt-right');
-        menu.classList.toggle('bx-x');
-        header_links.className = ('nav-links btn-2');
-        counter = 1;
-    } else {
-        menu.className = ('bx bx-menu-alt-right');
-        header_links.classList.remove('btn-2');
+  if (counter == 0) {
+    menu.classList.remove('bx-menu-alt-right');
+    menu.classList.toggle('bx-x');
+    header_links.className = ('nav-links btn-2');
+    counter = 1;
+  } else {
+    menu.className = ('bx bx-menu-alt-right');
+    header_links.classList.remove('btn-2');
 
-        header_links.className = ('nav-links btn-1');
-        counter = 0;
-    }
+    header_links.className = ('nav-links btn-1');
+    counter = 0;
+  }
 })
 
 window.addEventListener('resize', function () {
-    if (screen.width > 750) {
-        counter = 0;
-        header_links.classList.remove('btn-2');
-        header_links.className = ('nav-links btn-1');
+  if (screen.width > 750) {
+    counter = 0;
+    header_links.classList.remove('btn-2');
+    header_links.className = ('nav-links btn-1');
 
-    }
+  }
 })
 
 window.addEventListener('click', function (e) {
 
-    if (cerrado == false) {
-        let span = document.querySelector('header-header');
-        if (e.target == span) {
-            counter = 0;
-        }
+  if (cerrado == false) {
+    let span = document.querySelector('header-header');
+    if (e.target == span) {
+      counter = 0;
     }
+  }
 });
 
 
@@ -49,7 +47,10 @@ window.addEventListener('click', function (e) {
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Web Developer.", "Web Designer.", "Software Engineer.", "Web Developer.", "Web Designer.", "Software Engineer." , "Web Developer.", "Web Designer.", "Software Engineer."];
+const textArray = ["Web Developer.", "Web Designer.", "Software Engineer.",
+  "Web Developer.", "Web Designer.", "Software Engineer.",
+  "Web Developer.", "Web Designer.", "Software Engineer."
+];
 
 const typingDelay = 200;
 const erasingDelay = 100;
@@ -91,6 +92,6 @@ function erase() {
 }
 
 
-    document.addEventListener("DOMContentLoaded", function () {
-        if (textArray.length) setTimeout(type, newTextDelay + 250);
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
